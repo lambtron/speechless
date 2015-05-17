168cc0df5ad5b8b6351320ee288d43c5f5553b5e
Andy Jiang <andyjiang@gmail.com>
2015-05-17 14:28:54 -0700

    added giphy

--- client/js/app/index.js
+++ client/js/app/index.js
@@ -25,12 +25,30 @@ var App = component();
 export default App;
 
 /**
+ * After mount.
+ */
+
+App.prototype.afterMount = function(el, props, state) {
+  var setState = this.setState.bind(this);
+  var url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC';
+
+  request
+    .get(url)
+    .end(function(err, res) {
+      console.log(res.body);
+      setState({ imgUrl: res.body.data.image_original_url });
+  });
+};
+
+/**
  * Render `App`.
  */
 
 App.prototype.render = function(props, state) {
+  var imgUrl = state.imgUrl || '';
   return (
     <div>
+      <img src={imgUrl} style='width: 100%; height: 100%' />
     </div>
   );
 };
